190d28fe3ed797231f9c7cc46d08bbb47c8fb6be
Krisztian Szabo <thenonameguy24@gmail.com>
2024-02-28 03:11:16 +0100

    Fix current user check

--- create_schemamap_schema.sql
+++ create_schemamap_schema.sql
@@ -1,7 +1,7 @@
 -- check if the current user is schemamap
 do $$
 begin
-  if (select true from pg_roles where rolname = 'schemamap') is not true then
-    raise exception 'Please run this migration using the schemamap user so it automatically has correct ownership';
+  if (select current_role  = 'schemamap') is not true then
+    raise exception 'Please run this schema migration using the schemamap user so it automatically has correct ownership';
   end if;
 end $$;
