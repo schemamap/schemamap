5a00db2215b9ea37e568957c0793d5d0b729355b
Krisztian Szabo <thenonameguy24@gmail.com>
2024-04-12 10:57:31 +0200

    Add port-fwd-postgres script to Docker image

---
+++ docker/port-fwd-postgres.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Helper script to allow providing rathole client.toml files from outside of the container via exec:
+# docker exec -i schemamap-postgres port-fwd-postgres < rathole-client.toml
+
+read -rd "" stdin
+echo "$stdin" > /schemamap/last-rathole-config.toml
+exec rathole -c /schemamap/last-rathole-config.toml
