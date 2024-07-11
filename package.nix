c59d058162d2936728b6641ce8698fa93f0028b0
Krisztian Szabo <thenonameguy24@gmail.com>
2024-07-11 15:40:15 +0200

    Add pkg-config + openssl to package.nix

--- package.nix
+++ package.nix
@@ -7,12 +7,16 @@ pkgs.rustPlatform.buildRustPackage {
   src = pkgs.lib.cleanSource ./rust;
   cargoLock.lockFile = ./rust/Cargo.lock;
 
-  buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
+  buildInputs = [ pkgs.openssl ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
     frameworks.Security
     frameworks.CoreFoundation
     frameworks.CoreServices
     frameworks.SystemConfiguration
   ]);
+
+  nativeBuildInputs = [
+    pkgs.pkg-config # needed for opennssl to be found on Linux
+  ];
 
   meta = {
     description = "Schemamap.io CLI - Postgres Data Movement";
