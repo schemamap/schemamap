7d7caac40d5ca3c40614b6247dfdc43879271575
Krisztian Szabo <thenonameguy24@gmail.com>
2024-07-18 19:46:38 +0200

    Nicer TTY schemamap up and new deps

--- rust/src/up.rs
+++ rust/src/up.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
-use std::path::PathBuf;
+use std::{io::BufRead, path::PathBuf, process::exit};
 use tokio::{signal, sync::broadcast};
+use url::Url;
+use url_open::open;
 
 #[derive(Parser, Debug, Default, Clone)]
 pub(crate) struct UpArgs {
@@ -14,22 +16,109 @@ pub(crate) struct UpArgs {
     file: Option<PathBuf>,
 }
 
-fn fallback_rathole_file_path() -> PathBuf {
-    let path = directories::ProjectDirs::from("io", "schemamap", "schemamap-cli")
-        .expect("Failed to get project directories")
-        .config_dir()
-        .join("rathole.toml");
+fn personal_tunnel_file_path() -> PathBuf {
+    PathBuf::from(format!(".schemamap-tunnel.{}.toml", whoami::username()))
+}
 
-    log::info!("Using default rathole file path: {:?}", path);
+fn fallback_paths() -> Vec<PathBuf> {
+    vec![
+        PathBuf::from(".schemamap-tunnel.toml"),
+        personal_tunnel_file_path(),
+        dirs::config_dir().unwrap().join("schemamap-tunnel.toml"),
+        directories::ProjectDirs::from("io", "schemamap", "schemamap-cli")
+            .expect("Failed to get project directories")
+            .config_dir()
+            .join("tunnel.toml"),
+    ]
+}
 
-    PathBuf::from(path)
+fn read_multiline_string_from_stdin() -> Result<String> {
+    let stdin = std::io::stdin();
+    let mut input = String::new();
+    let mut lines = stdin.lock().lines();
+
+    let mut empty_line_count = 0;
+
+    while let Some(Ok(line)) = lines.next() {
+        if line.trim().is_empty() {
+            empty_line_count += 1;
+            if empty_line_count == 2 {
+                break;
+            }
+        } else {
+            empty_line_count = 0;
+        }
+        input.push_str(&line);
+        input.push('\n'); // Add newline character to preserve the original input format
+    }
+
+    let mut trimmed_input = input.trim_end().to_string();
+    trimmed_input.push('\n');
+    Ok(trimmed_input)
 }
 
 pub(crate) async fn up(args: UpArgs) -> Result<()> {
-    let rathole_file_path: PathBuf = args.file.unwrap_or_else(fallback_rathole_file_path);
+    let lookup_filepaths: Vec<PathBuf> = vec![args.file.clone()]
+        .into_iter()
+        .chain(fallback_paths().into_iter().map(Some))
+        .filter_map(|p| p.map(|p| p.to_path_buf()))
+        .collect();
 
+    let first_existing_filepath = lookup_filepaths
+        .iter()
+        .find(|p| {
+            log::debug!(
+                "Checking if tunnel config file exists: {}",
+                p.to_path_buf().display()
+            );
+            p.exists()
+        })
+        .ok_or_else(|| {
+          log::warn!("Couldn't find a tunnel config file in either of the following locations:");
+          for p in lookup_filepaths.iter() {
+            log::warn!("  {}", p.to_path_buf().display());
+          }
+          if atty::is(atty::Stream::Stdout) {
+            log::warn!("No existing tunnel config file found");
+            log::info!("Once you have received your tunnel config content, paste it here.");
+            log::info!("It will be saved to {}", personal_tunnel_file_path().display());
+            log::info!("It's recommended to .gitignore this file, as it's your personal tunnel config within this project.\n");
+
+            log::info!("Press enter to open the tunnel config creation page in your browser.");
+
+            let _ = std::io::stdin().read_line(&mut String::new());
+            open(&Url::parse("https://app.schemamap.io/").unwrap());
+            println!("Paste the tunnel config here then press Enter a few times:");
+            println!();
+
+            // read from stdin a multiline string, containing the tunnel config
+            let secret_tunnel_config = read_multiline_string_from_stdin().unwrap_or_else(|_| {
+                log::warn!("Failed to read tunnel config from stdin, exiting.");
+                exit(1);
+            });
+
+            println!();
+
+            // write the tunnel config to the file
+            std::fs::write(personal_tunnel_file_path(), secret_tunnel_config).unwrap();
+
+            println!();
+
+            log::info!("Tunnel config saved to {}", personal_tunnel_file_path().display());
+            log::info!("Run `schemamap up` again to start the tunnel.");
+            exit(0);
+          } else {
+            log::warn!("No existing tunnel config file found, go to https://app.schemamap.io/ to create one for this project's environment.");
+            log::info!("For a more assisted setup, run `schemamap up` in your terminal and follow the instructions.");
+            log::info!("Alternatively, provide the tunnel config content via the --file flag.");
+
+            exit(1)
+          }
+        });
+
+    let tunnel_file = first_existing_filepath.unwrap();
     let args = rathole::Cli {
-        config_path: Some(rathole_file_path),
+        config_path: Some(tunnel_file.to_path_buf()),
         genkey: None,
         client: true,
         server: false,
@@ -49,7 +138,10 @@ pub(crate) async fn up(args: UpArgs) -> Result<()> {
         }
     });
 
-    log::info!("Starting P2P encrypted Postgres tunnel towards Schemamap.io...");
+    log::info!(
+        "Starting P2P encrypted Postgres tunnel towards Schemamap.io, using: {}",
+        tunnel_file.display()
+    );
 
     rathole::run(args, shutdown_rx).await
 }
