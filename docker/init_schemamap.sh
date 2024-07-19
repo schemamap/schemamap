2a61db7ae454807a99541d159fdc82ca4672b2c4
Krisztian Szabo <thenonameguy24@gmail.com>
2024-07-19 08:27:06 +0200

    Use --dry-run within Docker image during PGDATA init

--- docker/init_schemamap.sh
+++ docker/init_schemamap.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Initializing Schemamap.io Postgres SDK for DB: $POSTGRES_DB"
 
-schemamap init --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --input=false
+schemamap init --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --input=false --dry-run | psql
 
 echo "Initialized Schemamap.io SDK 🎉"
