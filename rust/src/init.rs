1a882adf604a9e2e18acc254b8cf673484e7b12b
Krisztian Szabo <thenonameguy24@gmail.com>
2024-10-10 10:44:04 +0200

    Fix prompting for installing dev-time extensions

--- rust/src/init.rs
+++ rust/src/init.rs
@@ -1,5 +1,5 @@
 use crate::common::Cli;
-use anyhow::{Error, Result};
+use anyhow::Result;
 use clap::Args;
 use dialoguer::theme::ColorfulTheme;
 use tokio_postgres::{Client, Config, NoTls};
@@ -25,10 +25,11 @@ pub struct InitArgs {
     )]
     input: Option<bool>,
 
+    // Until https://github.com/clap-rs/clap/issues/5577 is addressed
+    // have to do this for falsey-by-default flags
     #[arg(long,
         help = "Install development-time extensions, like DB snapshot/restore.",
-        default_missing_value = "false",
-        default_value = "false",
+        default_missing_value = "true",
         num_args =0..=1,
         action = clap::ArgAction::Set
     )]
@@ -136,11 +137,12 @@ pub async fn install_dev_extensions(pgconfig: &Config, client: &Option<Client>) 
     // which is not allowed for CREATE DATABASE.
 
     let dbname = "schemamap_dev";
+    log::info!("Creating \"{}\" DB", dbname);
     let create_db_sql = format!("CREATE DATABASE {};", dbname);
     if let Some(c) = client {
         if let Err(e) = c.execute(&create_db_sql, &[]).await {
             log::warn!("Failed to create \"{}\" database: {}", dbname, e);
-            return Err(Error::msg("Failed to create dev database"));
+            return Ok(());
         } else {
             let mut dev_pgconfig = pgconfig.clone();
             let dev_pgconfig_dbname = dev_pgconfig.dbname(&dbname);
@@ -155,6 +157,8 @@ pub async fn install_dev_extensions(pgconfig: &Config, client: &Option<Client>) 
 
             if let Err(e) = schemamp_dev_c.batch_execute(SCHEMAMAP_DEV_SQL).await {
                 log::warn!("Failed to install dev extensions: {}", e);
+            } else {
+                log::info!("Installed dev extensions to \"{}\" DB", dbname);
             }
         }
     } else {
@@ -171,7 +175,6 @@ pub async fn init(cli: &Cli, args: &InitArgs) -> Result<()> {
 
     log::info!("Initializing Schemamap.io Postgres SDK");
 
-    let dev = args.dev.unwrap_or(false);
     // No reason to prompt for input if not interactive/TTY
     let interactive = atty::is(atty::Stream::Stdout) && args.input.unwrap_or(true);
 
@@ -225,11 +228,11 @@ pub async fn init(cli: &Cli, args: &InitArgs) -> Result<()> {
 
     log::info!("Schemamap.io Postgres SDK installed successfully");
 
-    let install_dev: bool;
-    if interactive && !dev && !dry_run {
+    let install_dev;
+    if interactive && args.dev.is_none() && !dry_run {
         install_dev = prompt_for_dev_installation();
     } else {
-        install_dev = dev;
+        install_dev = args.dev.unwrap_or(false);
     }
 
     if install_dev {
